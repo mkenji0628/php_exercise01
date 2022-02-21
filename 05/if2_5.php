5c8cf0fd02810e0d52174f2c75c5b24d72b90328
mkenji0628 <minamotojonisann@yahoo.co.jp>
2022-02-21 22:43:06 +0900

    課題5修正完了

--- 05/if2_5.php
+++ 05/if2_5.php
@@ -2,23 +2,26 @@
 
 $subjects = ['数学', '英語', '理科', '社会', '国語'];
 
+$n ='の試験は';
+$d ='です。';
+
 foreach ($subjects as $subject) {
 
     switch ($subject) {
         case '数学':
-            echo $subject . 'の試験は明日です。' . '<br>';
+            echo $subject .$n.'明日'.$d . '<br>';
             break;
         case '英語':
-            echo $subject . 'の試験は明後日です。' . '<br>';
+            echo $subject . $n . '明後日' . $d . '<br>';
             break;
         case '理科':
-            echo $subject . 'の試験は明々後日です。' . '<br>';
+            echo $subject . $n . '明々後日' . $d . '<br>';
             break;
         case '社会':
-            echo $subject . 'の試験は昨日です。' . '<br>';
+            echo $subject . $n . '昨日' . $d . '<br>';
             break;
         case '国語':
-            echo $subject . 'の試験は中止です。' . '<br>';
+            echo $subject . $n . '中止' . $d . '<br>';
             break;
     }
 }
