dc1507ac89a688348f1db4639ad49edf0777335c
mkenji0628 <minamotojonisann@yahoo.co.jp>
2022-02-21 20:54:30 +0900

    完成

--- 05/if2_5.php
+++ 05/if2_5.php
@@ -1,24 +1,29 @@
 <?php
 
-// $subjects = ['数学', '英語', '理科', '社会', '国語'];
-$subject = '数学';
-echo $subject;
+$subjects = ['数学', '英語', '理科', '社会', '国語'];
+// $subject = '国語';
+// echo $subject;
+
+foreach ($subjects as $subject) {
+    // echo $subject.'<br>';
 
 
 switch ($subject) {
     case '数学':
-        echo 'あなたのテストの点数は'.$subject.'点で、判定はＡです。';
+        echo $subject.'の試験は明日です。'.'<br>';
         break;
-    case '99':
-        echo 'あなたのテストの点数は'.$score.'点で、判定はＡです。';
+    case '英語':
+        echo $subject . 'の試験は明後日です。' . '<br>';
         break;
-    case '98':
-        echo 'あなたのテストの点数は'.$score.'点で、判定はＡです。';
+    case '理科':
+        echo $subject . 'の試験は明々後日です。' . '<br>';
         break;
-    case '97':
-        echo 'あなたのテストの点数は'.$score.'点で、判定はＡです。';
+    case '社会':
+        echo $subject . 'の試験は昨日です。' . '<br>';
         break;
-    case '96':
-        echo 'あなたのテストの点数は'.$score.'点で、判定はＡです。';
+    case '国語':
+        echo $subject . 'の試験は中止です。' . '<br>';
         break;
     }
+
+}
