4d8a545d79f8d19d49739cd16bc10c2afce9aacc
mkenji0628 <minamotojonisann@yahoo.co.jp>
2022-02-21 20:45:10 +0900

    変数固定なら完成

--- 05/if2_5.php
+++ 05/if2_5.php
@@ -1,4 +1,24 @@
 <?php
 
-$subjects = ['数学', '英語', '理科', '社会', '国語'];
+// $subjects = ['数学', '英語', '理科', '社会', '国語'];
+$subject = '数学';
+echo $subject;
 
+
+switch ($subject) {
+    case '数学':
+        echo 'あなたのテストの点数は'.$subject.'点で、判定はＡです。';
+        break;
+    case '99':
+        echo 'あなたのテストの点数は'.$score.'点で、判定はＡです。';
+        break;
+    case '98':
+        echo 'あなたのテストの点数は'.$score.'点で、判定はＡです。';
+        break;
+    case '97':
+        echo 'あなたのテストの点数は'.$score.'点で、判定はＡです。';
+        break;
+    case '96':
+        echo 'あなたのテストの点数は'.$score.'点で、判定はＡです。';
+        break;
+    }
