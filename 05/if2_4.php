eab07d49bdb21663208fdb696b16f0e325c56b6b
mkenji0628 <minamotojonisann@yahoo.co.jp>
2022-02-21 15:13:42 +0900

    PHPの基礎2の修正完了

---
+++ 05/if2_4.php
@@ -0,0 +1,18 @@
+<?php
+
+$score = ***;
+
+switch ($score) {
+    case '$score':
+        echo 'おはようございます';
+        break;
+    case '12':
+        echo 'こんにちは';
+        break;
+    case '18':
+        echo 'こんばんは';
+        break;
+    default:
+        echo 'どうも';
+        break;
+}
