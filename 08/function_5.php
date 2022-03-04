8d6a1746ff745323f82409fc1b1e3476013ca221
mkenji0628 <minamotojonisann@yahoo.co.jp>
2022-03-04 16:08:03 +0900

    一旦8-4まで提出

---
+++ 08/function_5.php
@@ -0,0 +1,21 @@
+<?php
+
+function today()
+{
+
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>課題8ー5</title>
+</head>
+<body>
+    
+</body>
+</html>
